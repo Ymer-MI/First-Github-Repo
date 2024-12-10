7f76c351f660bb3e43efbbcddef63b6bc06c9f4b
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-10 21:24:17 +0100

    Edited examples.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ document.getElementById("app")?.append(
     elements.heading.primary(`Test`, 'primary'), 
     elements.heading.secondary(`Testing that the element helpers are working`),
     elements.input('text', 'Placeholder'),
-    elements.input('email', 'ymer.nordstrom@medieinstitutet.se', true),
+    elements.input('email', 'some.email@mail.com', true),
     elements.input('checkbox', true, true),
     elements.button('Button')
   );
